31a759edc0d53f8215c87cc61ea2c658c1203542
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-21 18:14:25 -0300

    resolvido: exercicio 5

---
+++ IntroPoo/ExercicioCinco/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioCinco;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Aluno a = new Aluno();
+
+        Console.Write("Nome do Aluno: ");
+        a.Nome = Console.ReadLine();
+
+        Console.WriteLine("Digite as três notas do aluno:");
+        Console.WriteLine();
+
+        Console.Write("Nota 1: ");
+        a.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine();
+        Console.Write("Nota 2: ");
+        a.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine();
+        Console.Write("Nota 3: ");
+        a.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+        Console.WriteLine();
+        Console.WriteLine("Nota Final: " + a.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+
+        if (a.Aprovado())
+        {
+            Console.WriteLine("APROVADO!");
+        }
+        else
+        {
+            Console.WriteLine("REPROVADO!");
+            Console.WriteLine("FALTARAM " + a.NotaRestante().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
+
