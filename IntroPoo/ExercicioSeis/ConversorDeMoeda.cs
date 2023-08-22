07005272b191b3832322e8d071647c598d204037
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-21 21:31:26 -0300

    resolvido: exercicio 6

---
+++ IntroPoo/ExercicioSeis/ConversorDeMoeda.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ExercicioSeis
+{
+	public class ConversorDeMoeda
+	{
+		public static double Iof = 6.0;
+
+		public static double DolarParaReal(double quantia, double cotacao)
+		{
+			double conversao = quantia * cotacao;
+			return conversao + conversao * Iof / 100.0;
+		}
+	}
+}
+
---
+++ IntroPoo/ExercicioSeis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace ExercicioSeis;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Qual é a cotação do dólar: ");
+        double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine();
+
+        Console.Write("Quantos dólares você vai comprar: ");
+        double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine();
+
+        double conversaoTotal = ConversorDeMoeda.DolarParaReal(cotacao, quantia);
+
+        Console.WriteLine("Valor a pagar em reais: R$ " + conversaoTotal.ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
+
